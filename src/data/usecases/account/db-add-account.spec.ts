cd8d30af6c8928043fc8120b7d8f6eb1b513ac60
Philipe Herodek <herodek_10@hotmail.com>
2022-08-22 00:21:03 -0300

    test: ensure DbAddAccount throws if cryptographer throws

--- src/data/usecases/account/db-add-account.spec.ts
+++ src/data/usecases/account/db-add-account.spec.ts
@@ -1,6 +1,12 @@
 import { Cryptograph } from '@/data/protocols/cryptographer'
-import { AddAccount } from '@/domain/usecases/account/add-acount'
+import { AddAccount, AddAccountModel } from '@/domain/usecases/account/add-acount'
 import { DbAddAccount } from './db-add-account'
+
+const makeFakeAccount = (): AddAccountModel => ({
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_password'
+})
 
 const makeCryptoStub = (): Cryptograph => {
   class CryptographStub {
@@ -15,6 +21,7 @@ interface sutTypes {
   sut: AddAccount
   cryptographStub: Cryptograph
 }
+
 const makeSut = (): sutTypes => {
   const cryptographStub = makeCryptoStub()
   const sut = new DbAddAccount(cryptographStub)
@@ -27,12 +34,16 @@ describe('DbAddAccount Usecase', () => {
   test('Should call cryptographer with correct password', async () => {
     const { sut, cryptographStub } = makeSut()
     const cryptographySpy = jest.spyOn(cryptographStub, 'crypto')
-    const accountData = {
-      name: 'any_name',
-      email: 'any_email',
-      password: 'any_password'
-    }
+    const accountData = makeFakeAccount()
     await sut.add(accountData)
     expect(cryptographySpy).toHaveBeenCalledWith(accountData.password)
+  })
+
+  test('Should throw if Cryptographer throws', async () => {
+    const { sut, cryptographStub } = makeSut()
+    jest.spyOn(cryptographStub, 'crypto').mockRejectedValueOnce(new Error('Fake Error'))
+    const accountData = makeFakeAccount()
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow(new Error('Fake Error'))
   })
 })
