19cd9fd922f346233c7322f6fd53088e02b5d4a5
Philipe Herodek <herodek_10@hotmail.com>
2022-08-28 14:33:30 -0300

    refactor: remove unnused functions

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,23 +1,4 @@
-import { InternalServerError } from '../errors/'
 import { HttpResponse } from '../protocols'
-
-export const badRequest = (error: Error): HttpResponse<Error> => ({
-  statusCode: 400,
-  body: {
-    error,
-    success: false,
-    statusCode: 400
-  }
-})
-
-export const internalServerError = (): HttpResponse<Error> => ({
-  statusCode: 500,
-  body: {
-    error: new InternalServerError(),
-    statusCode: 500,
-    success: false
-  }
-})
 
 export const created = (body: any): HttpResponse<typeof body> => ({
   statusCode: 201,
