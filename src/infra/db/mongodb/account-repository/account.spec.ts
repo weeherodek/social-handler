42aa8972cd77dd47920a143cae69c98a3153597d
Philipe Herodek <herodek_10@hotmail.com>
2022-08-27 23:54:26 -0300

    test: ensure AccountMongoRepository returns correct value on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,32 @@
+import { MongoHelper } from '../helper/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+const makeSut = (): AccountMongoRepository => {
+  const sut = new AccountMongoRepository()
+  return sut
+}
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return a new account', async () => {
+    const sut = makeSut()
+    const newAccount = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    })
+    expect(newAccount).toBeDefined()
+    expect(newAccount.id).toBeDefined()
+    expect(newAccount.name).toBe('any_name')
+    expect(newAccount.email).toBe('any_email')
+    expect(newAccount.password).toBe('any_password')
+    expect(newAccount.date).toBeInstanceOf(Date)
+  })
+})
