5a82b7860377fd06b9e7f5f50a05d02cae596632
Philipe Herodek <herodek_10@hotmail.com>
2022-08-28 11:20:58 -0300

    test: add error handler

---
+++ src/main/middlewares/error-handler.test.ts
@@ -0,0 +1,46 @@
+import { InvalidParamError, InternalServerError, MissingParamError } from '@/presentation/errors'
+import request from 'supertest'
+import app from '../config/app'
+import { errorHandler } from './error-handler'
+
+describe('Error Handler Middleware', () => {
+  test('Should handle the exception when is a invalid param', async () => {
+    app.get('/test_invalid_param_error', () => {
+      throw new InvalidParamError('Custom Error')
+    }, errorHandler)
+    await request(app)
+      .get('/test_invalid_param_error')
+      .expect({ error: 'Invalid param: Custom Error' })
+      .expect(400)
+  })
+
+  test('Should handle the exception when is a missing param error', async () => {
+    app.get('/test_missing_param_error', () => {
+      throw new MissingParamError('Custom Error')
+    }, errorHandler)
+    await request(app)
+      .get('/test_missing_param_error')
+      .expect({ error: 'Missing param: Custom Error' })
+      .expect(400)
+  })
+
+  test('Should handle the exception when is a internal server error', async () => {
+    app.get('/test_internal_server_error', () => {
+      throw new InternalServerError()
+    }, errorHandler)
+    await request(app)
+      .get('/test_internal_server_error')
+      .expect({ error: 'Internal Server Error' })
+      .expect(500)
+  })
+
+  test('Should handle the exception when is a generic error', async () => {
+    app.get('/test_generic_error', () => {
+      throw new Error()
+    }, errorHandler)
+    await request(app)
+      .get('/test_generic_error')
+      .expect({ error: 'Internal Server Error' })
+      .expect(500)
+  })
+})
