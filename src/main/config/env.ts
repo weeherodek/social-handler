ac11a502060a992497aaebf2e6b697451886be75
Philipe Herodek <herodek_10@hotmail.com>
2022-08-28 23:51:25 -0300

    refactor: add collections environment variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/social-handler',
   port: process.env.PORT ?? 8080,
-  accountCollection: 'accounts',
-  templateCollection: 'templates',
-  messageCollection: 'messages'
+  accountCollection: process.env.ACCOUNT_COLLECTION ?? 'accounts',
+  templateCollection: process.env.TEMPLATE_COLLECTION ?? 'templates',
+  messageCollection: process.env.MESSAGE_COLLECTION ?? 'messages'
 }
