7377dbceb02a22359bde27aa70163ae29d96a6ba
Philipe Herodek <herodek_10@hotmail.com>
2022-08-28 23:48:12 -0300

    test: add template route to add new template

---
+++ src/main/routes/template-routes.test.ts
@@ -0,0 +1,40 @@
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import request from 'supertest'
+import app from '../config/app'
+import env from '../config/env'
+
+describe('Template Route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  afterEach(async () => {
+    const accountCollection = MongoHelper.getCollection(env.templateCollection)
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should call template route and return an new template', async () => {
+    await request(app)
+      .post('/api/template')
+      .send({
+        name: 'any_name',
+        text: 'any_text',
+        fields: [
+          {
+            name: 'any_field_name',
+            required: false
+          },
+          {
+            name: 'any_field_name_2',
+            required: true,
+            defaultValue: 'any_default_value'
+          }
+        ]
+      })
+      .expect(201)
+  })
+})
