42138709558c839d99fab1e0b95092aa4a008d8a
Philipe Herodek <herodek_10@hotmail.com>
2022-09-04 14:25:07 -0300

    refactor: group validation import in  a single file

--- src/main/factories/account/login-validation.spec.ts
+++ src/main/factories/account/login-validation.spec.ts
@@ -1,8 +1,5 @@
-import { EmailValidation } from '@/presentation/helpers/validators/email-validation'
-import { RequiredFieldValidation } from '@/presentation/helpers/validators/required-field-validation'
-import { TypeofValidation } from '@/presentation/helpers/validators/typeof-validation'
+import { EmailValidation, RequiredFieldValidation, TypeofValidation, ValidationComposite } from '@/presentation/helpers/validators/'
 import { Validation } from '@/presentation/protocols/validation'
-import { ValidationComposite } from '@/presentation/helpers/validators/validation-composite'
 import { EmailValidator } from '@/presentation/protocols/email-validator'
 import { makeLoginValidation } from './login-validation'
 
--- src/main/factories/account/login-validation.ts
+++ src/main/factories/account/login-validation.ts
@@ -1,8 +1,5 @@
-import { EmailValidation } from '@/presentation/helpers/validators/email-validation'
-import { RequiredFieldValidation } from '@/presentation/helpers/validators/required-field-validation'
-import { TypeofValidation } from '@/presentation/helpers/validators/typeof-validation'
+import { EmailValidation, RequiredFieldValidation, TypeofValidation, ValidationComposite } from '@/presentation/helpers/validators/'
 import { Validation } from '@/presentation/protocols/validation'
-import { ValidationComposite } from '@/presentation/helpers/validators/validation-composite'
 import { EmailValidatorAdapter } from '@/utils/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
