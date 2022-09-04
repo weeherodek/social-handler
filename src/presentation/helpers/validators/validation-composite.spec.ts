a02104fe9d894f25d4a2a75ec66db74dd3492da6
Philipe Herodek <herodek_10@hotmail.com>
2022-09-04 12:45:55 -0300

    test: ensure ValidationComposite makes the correct message

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,24 +1,57 @@
+import { InvalidParamError, MissingParamError } from '@/presentation/errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (data: any): string | null {
-      return new Error('Error Message').message
+      return null
     }
   }
   return new ValidationStub()
 }
 
-const makeSut = (): ValidationComposite => {
-  const validation = makeValidation()
-  return new ValidationComposite([validation])
+interface sutTypes {
+  validationStubs: Validation[]
+  sut: ValidationComposite
+}
+
+const makeSut = (): sutTypes => {
+  const validationStubs = [makeValidation(), makeValidation()]
+  const sut = new ValidationComposite(validationStubs)
+  return {
+    sut,
+    validationStubs
+  }
 }
 
 describe('Validation Composite', () => {
-  test('Should returns the erros found in the composite', () => {
-    const sut = makeSut()
+  test('Should returns the errors found in the composite', () => {
+    const { sut, validationStubs: validationStub } = makeSut()
+    jest.spyOn(validationStub[0], 'validate').mockReturnValueOnce(new Error('Error Message').message)
     const error = sut.validate({ field: 'any_field' })
     expect(error).toBe(new Error('Error Message').message)
+  })
+
+  test('Should return the error found in the composite second index', () => {
+    const { sut, validationStubs: validationStub } = makeSut()
+    jest.spyOn(validationStub[1], 'validate').mockReturnValueOnce(new InvalidParamError('Error Message').message)
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBe(new InvalidParamError('Error Message').message)
+  })
+
+  test('Should return the error found in the composite second index', () => {
+    const { sut, validationStubs: validationStub } = makeSut()
+
+    jest.spyOn(validationStub[0], 'validate').mockReturnValueOnce(new MissingParamError('Error Message').message)
+    jest.spyOn(validationStub[1], 'validate').mockReturnValueOnce(new InvalidParamError('Error Message').message)
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBe([new MissingParamError('Error Message').message, new InvalidParamError('Error Message').message].join(', '))
+  })
+
+  test('Should return null if composite succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBe('')
   })
 })
