e07d0e43bb8c5fddb0fc82bd793f444ad88e5bba
Philipe Herodek <herodek_10@hotmail.com>
2022-09-04 09:34:47 -0300

    feat: ensure LoginController validate email field

--- src/presentation/controllers/account/login.ts
+++ src/presentation/controllers/account/login.ts
@@ -1,14 +1,27 @@
-import { MissingParamError } from '@/presentation/errors'
+import { InvalidParamError, MissingParamError } from '@/presentation/errors'
 import { Controller } from '@/presentation/protocols/controller'
+import { EmailValidator } from '@/presentation/protocols/email-validator'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 
 export class LoginController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {
+
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['email', 'password']
     for (const field of requiredFields) {
       if (httpRequest.body[field] === undefined) {
         throw new MissingParamError(field)
       }
+    }
+
+    const { email } = httpRequest.body
+
+    const isValid = this.emailValidator.isValid(email)
+
+    if (!isValid) {
+      throw new InvalidParamError('email')
     }
 
     return await Promise.resolve({
