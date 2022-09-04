f552bbb747cea2557cb081a063bf6eb519dc6f23
Philipe Herodek <herodek_10@hotmail.com>
2022-09-04 13:08:48 -0300

    test: ensure Typeof validation validate the  type of the field

--- src/main/factories/account/signup-validation.spec.ts
+++ src/main/factories/account/signup-validation.spec.ts
@@ -1,6 +1,7 @@
 import { CompareFieldsValidation } from '@/presentation/helpers/validators/compare-fields-validation'
 import { EmailValidation } from '@/presentation/helpers/validators/email-validation'
 import { RequiredFieldValidation } from '@/presentation/helpers/validators/required-field-validation'
+import { TypeofValidation } from '@/presentation/helpers/validators/typeof-validation'
 import { Validation } from '@/presentation/helpers/validators/validation'
 import { ValidationComposite } from '@/presentation/helpers/validators/validation-composite'
 import { EmailValidator } from '@/presentation/protocols/email-validator'
@@ -23,6 +24,7 @@ describe('SignupValidation Factory', () => {
     const validations: Validation[] = []
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field))
+      validations.push(new TypeofValidation(field, 'string'))
     }
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     validations.push(new EmailValidation('email', makeEmailValidator()))
