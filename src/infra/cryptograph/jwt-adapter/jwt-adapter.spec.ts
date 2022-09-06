760f44fc38335e2179c3a8bb77d4069a89dc8ab7
Philipe Herodek <herodek_10@hotmail.com>
2022-09-05 22:37:10 -0300

    test: ensure JwtAdapter returns the correct value

--- src/infra/cryptograph/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptograph/jwt-adapter/jwt-adapter.spec.ts
@@ -1,13 +1,21 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
-jest.mock('jsonwebtoken')
+jest.mock('jsonwebtoken', () => ({
+  sign: () => 'any_token'
+}))
 
 describe('JWT Adapter', () => {
-  test('Should call sign with correct values', async () => {
+  test('Should call sign with correct values', () => {
     const sut = new JwtAdapter('secret')
     const signSpy = jest.spyOn(jwt, 'sign')
     sut.encrypt('any_value')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
+  })
+
+  test('Should return a token on success', () => {
+    const sut = new JwtAdapter('secret')
+    const token = sut.encrypt('any_value')
+    expect(token).toBe('any_token')
   })
 })
