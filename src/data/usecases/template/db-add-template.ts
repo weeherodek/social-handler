5edbd08c067f00df8c8403c88d0c39089dd56ddd
Philipe Herodek <herodek_10@hotmail.com>
2022-09-06 19:34:23 -0300

    feat: ensure DbAddTemplate calls loadByName with correct value

--- src/data/usecases/template/db-add-template.ts
+++ src/data/usecases/template/db-add-template.ts
@@ -1,11 +1,16 @@
 import { AddTemplateRepository } from '@/data/protocols/db/template/add-template-repository'
+import { LoadTemplateByNameRepository } from '@/data/protocols/db/template/load-template-by-name-repository'
 import { TemplateModel } from '@/domain/models/template/template'
 import { AddTemplate, AddTemplateModel } from '@/domain/usecases/template/add-template'
 
 export class DbAddTemplate implements AddTemplate {
-  constructor (private readonly addTemplateRepository: AddTemplateRepository) {}
+  constructor (
+    private readonly addTemplateRepository: AddTemplateRepository,
+    private readonly loadTemplateByNameRepository: LoadTemplateByNameRepository
+  ) {}
 
   async add (template: AddTemplateModel): Promise<TemplateModel> {
+    await this.loadTemplateByNameRepository.loadByName(template.name)
     const newTemplate = await this.addTemplateRepository.add(template)
     return newTemplate
   }
--- src/domain/usecases/template/add-template.ts
+++ src/domain/usecases/template/add-template.ts
@@ -1,7 +1,7 @@
 import { TemplateModel } from '@/domain/models/template/template'
 
 export interface AddTemplate {
-  add: (template: AddTemplateModel) => Promise<TemplateModel>
+  add: (template: AddTemplateModel) => Promise<TemplateModel | null>
 }
 
 export interface AddTemplateModel {
