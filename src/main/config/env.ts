8b7319de7785ebd13b0547d1ea38a8148b5b63bd
Philipe Herodek <herodek_10@hotmail.com>
2022-09-06 20:34:37 -0300

    feat: add login  route

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,7 @@ export default {
   accountCollection: process.env.ACCOUNT_COLLECTION ?? 'accounts',
   templateCollection: process.env.TEMPLATE_COLLECTION ?? 'templates',
   messageCollection: process.env.MESSAGE_COLLECTION ?? 'messages',
-  errorLogCollection: process.env.ERROR_LOG_COLLECTION ?? 'errorsLog'
+  errorLogCollection: process.env.ERROR_LOG_COLLECTION ?? 'errorsLog',
+  salt: Number(process.env.SALT) ?? 12,
+  jwtSecret: process.env.JWT_SECRET ?? 'AjiuAJdijaij$iaijda@i12312CasokPS'
 }
--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignupController } from '../factories/account/signup'
+import { makeSignupController } from '../factories/account/signup-factory'
+import { makeLoginController } from '../factories/account/login-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignupController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
