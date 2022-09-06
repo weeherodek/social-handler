f0f5963f623d27ea487682676714e14f967f81dc
Philipe Herodek <herodek_10@hotmail.com>
2022-09-06 19:45:02 -0300

    feat: ensure throw AlreadyExistsError if template already exists

--- src/presentation/controllers/template/save-template.ts
+++ src/presentation/controllers/template/save-template.ts
@@ -1,5 +1,6 @@
 import { TemplateModel } from '@/domain/models/template/template'
 import { AddTemplate, AddTemplateModel } from '@/domain/usecases/template/add-template'
+import { AlreadyExistsError } from '@/presentation/errors/already-exists-error'
 import { created } from '@/presentation/helpers/http/http-helper'
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
@@ -11,6 +12,7 @@ export class AddTemplateController implements Controller {
 
   async handle (httpRequest: HttpRequest<AddTemplateModel>): Promise<HttpResponse<TemplateModel>> {
     const newTemplate = await this.addTemplate.add({ ...httpRequest.body })
-    return created(newTemplate)
+    if (newTemplate) return created(newTemplate)
+    throw new AlreadyExistsError('Template', httpRequest.body.name)
   }
 }
