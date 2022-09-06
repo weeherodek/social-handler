d2c09721318322c6937d96b6e38292abd2df0fa5
Philipe Herodek <herodek_10@hotmail.com>
2022-09-05 22:42:27 -0300

    refactor: move duplicated code to a factory method

--- src/infra/cryptograph/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptograph/jwt-adapter/jwt-adapter.spec.ts
@@ -5,22 +5,26 @@ jest.mock('jsonwebtoken', () => ({
   sign: () => 'any_token'
 }))
 
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
+
 describe('JWT Adapter', () => {
   test('Should call sign with correct values', () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     sut.encrypt('any_value')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
   })
 
   test('Should return a token on success', () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const token = sut.encrypt('any_value')
     expect(token).toBe('any_token')
   })
 
   test('Should throw if jwt throws', () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
       throw new Error('Fake Error')
     })
