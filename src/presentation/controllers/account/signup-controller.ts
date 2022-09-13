1b6f31caf9b539d93916722cede8117858d9a513
Philipe Herodek <herodek_10@hotmail.com>
2022-09-12 21:40:36 -0300

    feat: ensure signup controller calls auth with correct values

--- src/presentation/controllers/account/signup-controller.ts
+++ src/presentation/controllers/account/signup-controller.ts
@@ -4,16 +4,19 @@ import { created } from '@/presentation/helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { Controller } from '@/presentation/protocols/controller'
 import { AlreadyExistsError } from '@/presentation/errors/already-exists-error'
+import { Authentication } from '@/domain/usecases/account/authentication'
 
 export class SignUpController implements Controller {
   constructor (
-    private readonly addAccount: AddAccount
+    private readonly addAccount: AddAccount,
+    private readonly authentication: Authentication
   ) {}
 
   async handle (httpRequest: HttpRequest<AddAccountModel>): Promise<HttpResponse<AccountModel>> {
     const { email, password, name } = httpRequest.body
     const account = await this.addAccount.add({ name, email, password })
     if (account) {
+      await this.authentication.auth({ email, password })
       return created(account)
     }
     throw new AlreadyExistsError('User', email)
