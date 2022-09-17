d60c7a0f6f9aa8b63c3fb9fee16a40aaffac6417
Philipe Herodek <herodek_10@hotmail.com>
2022-09-16 22:01:55 -0300

    feat: add dev file to run app

---
+++ src/main/server-dev.ts
@@ -0,0 +1,8 @@
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl).then(async () => {
+  const app = (await import ('./config/app')).default
+  const port = env.port
+  app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+}).catch(console.error)
