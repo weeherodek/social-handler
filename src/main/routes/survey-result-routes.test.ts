a95af09290c61cdea138363d6a9fbce5763a909d
Philipe Herodek <herodek_10@hotmail.com>
2022-09-23 21:21:17 -0300

    test: make save survey result controller factory

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,4 +1,5 @@
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import app from '../config/app'
@@ -7,6 +8,24 @@ import env from '../config/env'
 let surveyCollection: Collection
 let accountCollection: Collection
 let surveyResultCollection: Collection
+
+const makeValidAcessToken = async (): Promise<string> => {
+  const { insertedId } = await accountCollection.insertOne({
+    name: 'Philipe',
+    email: 'philipe.herodek@email.com.br',
+    password: 'any_password'
+  })
+
+  const accessToken = sign({ id: insertedId.toString() }, env.jwtSecret)
+
+  await accountCollection.updateOne({ _id: insertedId }, {
+    $set: {
+      accessToken
+    }
+  })
+
+  return accessToken
+}
 
 describe('Survey Result Routes', () => {
   beforeAll(async () => {
@@ -32,6 +51,25 @@ describe('Survey Result Routes', () => {
         .put('/api/surveys/123/results')
         .send({ answer: 'Answer 1' })
         .expect(401)
+    })
+
+    test('Should return 200 on save survey result with valid x-access-token', async () => {
+      const accessToken = await makeValidAcessToken()
+      const insertedSurvey = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const surveyId = insertedSurvey.insertedId.toString()
+      await request(app)
+        .put(`/api/surveys/${surveyId}/results`)
+        .set('x-access-token', accessToken)
+        .send({ answer: 'Answer 1' })
+        .expect(200)
     })
   })
 })
