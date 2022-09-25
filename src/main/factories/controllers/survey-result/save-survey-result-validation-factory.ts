fa1baa756e318e8256aeba1d593d6023827fbc80
Philipe Herodek <herodek_10@hotmail.com>
2022-09-25 20:23:03 -0300

    feat: add Mongo Id validation on Save Survey Result controller

--- src/main/factories/controllers/survey-result/save-survey-result-validation-factory.ts
+++ src/main/factories/controllers/survey-result/save-survey-result-validation-factory.ts
@@ -1,5 +1,7 @@
 import { RequiredFieldValidation, TypeofValidation, ValidationComposite } from '@/validation/validators'
 import { Validation } from '@/presentation/protocols/validation'
+import { MongoIdValidation } from '@/validation/validators/mongoid-validation'
+import { MongoIdValidatorAdapter } from '@/infra/validators/mongo-id-validator-adapter'
 
 export const makeSaveSurveyResultValidation = (): ValidationComposite => {
   const validations: Validation[] = []
@@ -7,6 +9,6 @@ export const makeSaveSurveyResultValidation = (): ValidationComposite => {
     validations.push(new RequiredFieldValidation(field))
     validations.push(new TypeofValidation(field, 'string'))
   }
-
+  validations.push(new MongoIdValidation('surveyId', new MongoIdValidatorAdapter()))
   return new ValidationComposite(validations)
 }
