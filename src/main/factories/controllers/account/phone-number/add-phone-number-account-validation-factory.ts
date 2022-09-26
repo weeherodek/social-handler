f4bbcce4bf698dde9db13e57c572a4c87995148d
Philipe Herodek <herodek_10@hotmail.com>
2022-09-26 01:49:33 -0300

    feat: add mobile phone validation on AddPhoneNumberAccount

--- src/main/factories/controllers/account/phone-number/add-phone-number-account-validation-factory.ts
+++ src/main/factories/controllers/account/phone-number/add-phone-number-account-validation-factory.ts
@@ -1,11 +1,14 @@
 import { RequiredFieldValidation, TypeofValidation, ValidationComposite } from '@/validation/validators'
 import { Validation } from '@/presentation/protocols/validation'
+import { PhoneNumberValidation } from '@/validation/validators/phone-number-validation'
+import { PhoneNumberValidatorAdapter } from '@/infra/validators/phone-number-validator-adapter'
 
 export const makeAddPhoneNumberAccountValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['phoneNumber']) {
     validations.push(new RequiredFieldValidation(field))
     validations.push(new TypeofValidation(field, 'string'))
+    validations.push(new PhoneNumberValidation(field, new PhoneNumberValidatorAdapter()))
   }
   return new ValidationComposite(validations)
 }
